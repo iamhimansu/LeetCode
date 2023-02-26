a89953c69e6cf9f4f81c9f8341349986b56b05df
Himanshu Raj <44632846+iamhimansu@users.noreply.github.com>
2023-02-26 15:18:19 +0530

    Update and rename Second Highest Salary to Second Highest Salary.sql

--- sql/Second Highest Salary.sql
+++ sql/Second Highest Salary.sql
@@ -2,6 +2,4 @@ Write an SQL query to report the second highest salary from the Employee table. 
 
 
 Answer: 
-# Write your MySQL query statement below
-
 SELECT IFNULL((SELECT distinct salary FROM Employee ORDER BY salary DESC LIMIT 1 OFFSET 1), null) as 'SecondHighestSalary'
